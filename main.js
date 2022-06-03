5c7a7285019b79bf8073460a11c3554243ca6e01
saim514 <81713694+saim514@users.noreply.github.com>
2022-06-03 17:48:03 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ screen_height = 0;
 to_number = "";
 apple = "";
 draw_apple = "";
+speak_data = "";
+
+function preload()
+{
+  apple = loadImage('apple.png');
+}
 
 var SpeechRecognition = window.webkitSpeechRecognition;
   
@@ -27,36 +33,33 @@ recognition.onresult = function(event) {
     to_number = Number(content);
 
     if(Number.isInteger(to_number)){
-    draw_apple = "set";
+      document.getElementById("status").innerHTML = "Started drawing apple(s)";
+      draw_apple = "set";
     } else{
       document.getElementById("status").innerHTML = "The speech has not recognized a number";
     }
-}
-
-function preload()
-{
-  loadImage('apple.png');
-  apple = "apple.png";
 }
 
 function setup() {
  screen_width = window.innerWidth;
  screen_height = window.innerHeight;
  canvas = createCanvas(screen_width, screen_height - 150);
+ canvas.position(Math.random(01 - 150))
 }
 
 function draw() {
   if(draw_apple == "set")
   {
-    document.getElementById("status").innerHTML = to_number + " Apples drawn";
-    draw_apple = "";
-
     for(var i = 1; i <= to_number; i++)
     {
       x = Math.floor(Math.random() * 700);
       y = Math.floor(Math.random() * 400);
       image(apple, x, y, 50, 50);
     }
+    document.getElementById("status").innerHTML = to_number + " Apple(s) drawn";
+    speak_data = to_number + "apple(s) drawn";
+    speak();
+    draw_apple = "";
   }
 }
 
@@ -67,7 +70,6 @@ function speak(){
 
     synth.speak(utterThis);
 
-    speak_data = to_number + " Apples drawn";
+    speak_data = "";
 
-    speak();
 }
